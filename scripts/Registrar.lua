c85709da9ed1f8be0ddd22839f7284bf46a5aafd
Steven <github@steven-h.net>
2022-05-22 18:45:03 -0400

    Document and fix components

--- scripts/Registrar.lua
+++ scripts/Registrar.lua
@@ -3,7 +3,7 @@ local Components = require "necro.game.data.Components"
 Components.register {
   Switcheroo_noGive = {},
   Switcheroo_noTake = {
-    Components.constant.bool("onlyIfGiven", false),
+    Components.constant.bool("unlessGiven", false),
     Components.field.bool("wasGiven", false)
   }
 }
