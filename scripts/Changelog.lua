6c16b81cbc9714348676e159dfb861943ffa500f
Steven <github@steven-h.net>
2023-08-27 04:47:43 -0400

    Add changelogs

---
+++ scripts/Changelog.lua
@@ -0,0 +1,17 @@
+local Menu     = require "necro.menu.Menu"
+local Settings = require "necro.config.Settings"
+
+Settings.user.action {
+  name = "View Switcheroo changelog",
+  desc = "View changelogs for Switcheroo mod",
+  id = "changelog",
+  order = 0,
+  autoRegister = true,
+  action = function()
+    Menu.open("changeLog", {
+      fileNames = { "3.4.0.md", "3.3.x.md", "3.x.x.md", "2.0.0.md", "1.x.x.md" },
+      basePath = "mods/Switcheroo/changelogs/",
+      index = 1
+    })
+  end
+}
