912051264f2dadbaba8aba1f82e966955cd5738a
Steven <github@steven-h.net>
2023-09-15 10:22:41 -0400

    Update changelogs for today's release

---
+++ changelogs/latest.md
@@ -0,0 +1,6 @@
+# **Version 3.3.3**
+>## **2023-09-15**
+
+- **Added** in-game changelogs under `Options > Mod options > Switcheroo > View Switcheroo changelog`
+- **Added** in-game option presets under `Customize > Custom rules > Mod options > Switcheroo > Use a preset`
+- **Reorganized** the mod custom rules menu to present fewer options at once
--- scripts/Changelog.lua
+++ scripts/Changelog.lua
@@ -9,7 +9,7 @@ Settings.user.action {
   autoRegister = true,
   action = function()
     Menu.open("changeLog", {
-      fileNames = { "3.4.0.md", "3.3.x.md", "3.x.x.md", "2.0.0.md", "1.x.x.md" },
+      fileNames = { "latest.md", "3.3.x.md", "3.x.x.md", "2.0.0.md", "1.x.x.md" },
       basePath = "mods/Switcheroo/changelogs/",
       index = 1
     })
